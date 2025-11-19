7d57f06fb91f116cda6d57faf38d93ee1e204509
poom-ytthpch <yutthapichai.cha@ext.axonstech.com>
2025-11-19 15:28:22 +0700

    add headers return

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,48 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
-import { IntrospectAndCompose } from '@apollo/gateway';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+
     GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
+
       server: {
         playground: false,
         plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        context: ({ req }) => ({
+          headers: req.headers,
+        }),
       },
+
       gateway: {
+        buildService: ({ url }) => {
+          return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({ request, context }) {
+              if (context.headers) {
+                Object.entries(context.headers).forEach(([key, value]) => {
+                  request.http?.headers.set(key, String(value)); // forward header
+                });
+              }
+            },
+          });
+        },
+
         supergraphSdl: new IntrospectAndCompose({
           subgraphs: [
-            {
-              name: 'auth',
-              url: process.env.AUTH_SERVICE_URL,
-            },
-            {
-              name: 'wallet',
-              url: process.env.WALLET_SERVICE_URL,
-            },
-            {
-              name: 'charger',
-              url: process.env.CHARGER_SERVICE_URL,
-            },
+            { name: 'auth', url: process.env.AUTH_SERVICE_URL },
+            { name: 'wallet', url: process.env.WALLET_SERVICE_URL },
+            { name: 'charger', url: process.env.CHARGER_SERVICE_URL },
           ],
         }),
       },
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
